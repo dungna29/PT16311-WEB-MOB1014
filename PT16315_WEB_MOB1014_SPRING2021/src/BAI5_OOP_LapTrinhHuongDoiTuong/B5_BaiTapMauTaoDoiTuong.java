08de3ca30339c5fec1cdc537490dccc69502a3c2
Nguyễn Anh Dũng <dungna29@fpt.edu.vn>
2021-02-23 16:23:32 +0700

    1

--- PT16315_WEB_MOB1014_SPRING2021/src/BAI5_OOP_LapTrinhHuongDoiTuong/B5_BaiTapMauTaoDoiTuong.java
+++ PT16315_WEB_MOB1014_SPRING2021/src/BAI5_OOP_LapTrinhHuongDoiTuong/B5_BaiTapMauTaoDoiTuong.java
@@ -27,7 +27,30 @@ public class B5_BaiTapMauTaoDoiTuong {
         B5_BaiTapMauTaoDoiTuong b5 = new B5_BaiTapMauTaoDoiTuong();
         //b5.tao1DoiTuong();
         //b5.taoNhieuDoiTuong();
-        b5.taoNhieuDoiTuongNangCao();
+        //b5.taoNhieuDoiTuongNangCao();
+
+        //Nhập 1 dãy điểm môn C# theo nhu cầu người sử dụng và in tất cả các điểm có điểm số dưới 5 và điểm số cao nhất trong dãy. 
+        //(Sử dụng List để lưu điểm) – (5 điểm)
+        Scanner sc = new Scanner(System.in);
+        List<Double> lstDiemCsharp = new ArrayList<>();
+        int input = 0;
+        System.out.println("Bạn muốn nhập vào bao nhiêu điểm C#1: ");
+        input = Integer.parseInt(sc.nextLine());//Lấy số lượng điểm người dùng muốn nhập
+        for (int i = 0; i < input; i++) {
+            System.out.println("Mời bạn nhập điểm thứ: " + i);
+            lstDiemCsharp.add(Double.parseDouble(sc.nextLine()));
+        }
+        System.out.println("Tất cả các điểm số dưới 5: ");
+        double max = lstDiemCsharp.get(0);
+        for (Double x : lstDiemCsharp) {
+            if (x < 5) {
+                System.out.print(x + " ");
+            }
+            if (max < x) {
+                max = x;
+            }
+        }
+        System.out.println("Điểm số cao nhất trong dãy nhập vào là: " + max);
 
     }
 
@@ -60,36 +83,38 @@ public class B5_BaiTapMauTaoDoiTuong {
             System.out.print("Mời bạn nhập tên TUỔI: ");
             _giaoVien.setTuoi(Integer.parseInt(_sc.nextLine()));
             System.out.print("Mời bạn nhập tên Giới Tính: ");
-            _giaoVien.setGioiTinh(_sc.nextLine());           
+            _giaoVien.setGioiTinh(_sc.nextLine());
             //Thêm đối tượng vào trong List sau khi nhập đủ thông tin của 1 đối tượng
             _lstGiaoViens.add(_giaoVien);
         }
-        
+
         //Xuất danh sách nhiều phần tử trong List dùng vòng lặp
-        for (GiaoVien x : _lstGiaoViens) {            
-             System.out.println(x.toString());
+        for (GiaoVien x : _lstGiaoViens) {
+            System.out.println(x.toString());
         }
     }
+
     void taoNhieuDoiTuongNangCao() {
         System.out.print("Mời bạn nhập số lượng GV muốn thêm: ");
         _input = _sc.nextLine();
         for (int i = 0; i < Integer.parseInt(_input); i++) {
             _giaoVien = new GiaoVien();
-            _giaoVien.setTenGV(getInput("Tên Giáo Viên"));          
-            _giaoVien.setMaGV(getInput("Tên Mã Giáo Viên"));          
-            _giaoVien.setSdt(Long.parseLong(getInput("Số điện thoại")));            
-            _giaoVien.setTuoi(Integer.parseInt(getInput("Tuổi")));          
-            _giaoVien.setGioiTinh(getInput("Giới Tính"));           
+            _giaoVien.setTenGV(getInput("Tên Giáo Viên"));
+            _giaoVien.setMaGV(getInput("Tên Mã Giáo Viên"));
+            _giaoVien.setSdt(Long.parseLong(getInput("Số điện thoại")));
+            _giaoVien.setTuoi(Integer.parseInt(getInput("Tuổi")));
+            _giaoVien.setGioiTinh(getInput("Giới Tính"));
             //Thêm đối tượng vào trong List sau khi nhập đủ thông tin của 1 đối tượng
             _lstGiaoViens.add(_giaoVien);
-        }        
+        }
         //Xuất danh sách nhiều phần tử trong List dùng vòng lặp
-        for (GiaoVien x : _lstGiaoViens) {            
-             System.out.println(x.toString());
+        for (GiaoVien x : _lstGiaoViens) {
+            System.out.println(x.toString());
         }
     }
-    String getInput(String name){
-        System.out.printf("Mời bạn nhập %s: ",name);
+
+    String getInput(String name) {
+        System.out.printf("Mời bạn nhập %s: ", name);
         return _sc.nextLine();
     }
 }
